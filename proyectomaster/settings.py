33b0e37bb045a884429ef2c6cfb1e176695c2bcf
sk8walker <sk8walker@msn.com>
2014-04-06 19:11:10 -0500

    Proyecto Maestro desarrollado en Django v 1.4.10

--- proyectomaster/settings.py
+++ proyectomaster/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dbase',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -113,12 +113,37 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
+    # Defaults
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps.
+    'apimonitors',
+    'apps',
+    'banners',
+    'calendars',
+    'chats',
+    'directorys',
+    'forums',
+    'geolocalizations',
+    'hotels',
+    'jobboards',
+    'magazines',
+    'netlabels',
+    'newspapers',
+    'photobooks',
+    'pollings',
+    'radios',
+    'realstates',
+    'restaurants',
+    'schools',
+    'sites',
+    'stores',
+    'tvs',
+    'userscontrols',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
